18a635192fec6dd37d65abefe168cb086972962f
carol <carolmirabal27@gmail.com>
2019-08-12 22:25:49 -0400

    first commit

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
-// Utilizar funcionalidades del Ecmascript 6
+
 'use strict'
-// Cargamos el módulo de mongoose para poder conectarnos a MongoDB
+
 var mongoose = require('mongoose');
-// *Cargamos el fichero app.js con la configuración de Express
+
 var app = require('./app');
-// Creamos la variable PORT para indicar el puerto en el que va a funcionar el servidor
-var port = 3800;
-// Le indicamos a Mongoose que haremos la conexión con Promesas
+
+var port = 27017;
+
 mongoose.Promise = global.Promise;
-// Usamos el método connect para conectarnos a nuestra base de datos
-mongoose.connect('mongodb://localhost:27017/curso_mean_social',  { useMongoClient: true})
+
+mongoose.connect('mongodb+srv://admin-mongo_13:19sxcGG0MbCE5bLx@cluster0-qpjgr.mongodb.net/test?retryWrites=true&w=majority',  { useMongoClient: true})
     .then(() => {
-        // Cuando se realiza la conexión, lanzamos este mensaje por consola
+       
         console.log("La conexión a la base de datos curso_mean_social se ha realizado correctamente")
-    
-        // CREAR EL SERVIDOR WEB CON NODEJS
+
         app.listen(port, () => {
-            console.log("servidor corriendo en http://localhost:3800");
+            console.log("servidor corriendo en atlas");
         });
     })
-    // Si no se conecta correctamente escupimos el error
+    
     .catch(err => console.log(err));
