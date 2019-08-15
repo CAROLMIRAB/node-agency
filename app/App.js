99dce31d5984fe55964704a25ce4e1b52f68757c
carol <carolmirabal27@gmail.com>
2019-08-15 19:50:26 -0400

    react

--- app/App.js
+++ app/App.js
@@ -3,6 +3,12 @@ import React, { Component } from 'react';
 
 
 class App extends Component {
+
+    constructor(){
+        super();
+        
+    }
+
     render(){
         return {
             <div> hola mundo </div>
