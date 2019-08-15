4f4b85c79cef098e22f4b0e1c2103b59a5d46b4d
carol <carolmirabal27@gmail.com>
2019-08-15 19:39:52 -0400

    react

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var user_routes = require('./routes/user');
 //un metodo que se ejecuta antes que llegue a un controlador
 //Configuramos bodyParser para que convierta el body de nuestras peticiones a JSON
 app.use(bodyParser.urlencoded({extended:false}));
-app.use(bodyParser.json());
+app.use(express.json());
 // Cargamos las rutas
 app.use('/api', user_routes);
 // exportamos este módulo para poder usar la variable app fuera de este archivo
