02c1fbf7d1d3577d76ed68e0abaff3a5ece8cc8b
Franklin Sánchez <49462365+booba302@users.noreply.github.com>
2019-10-08 20:11:56 -0400

    ultima cosa

--- index.js
+++ index.js
@@ -5,17 +5,17 @@ var mongoose = require('mongoose');
 
 var app = require('./app');
 
-var port = 3008;
+var port = 27017;
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb+srv://admin-mongo_13:19sxcGG0MbCE5bLx@cluster0-qpjgr.mongodb.net/test?retryWrites=true&w=majority',  { useNewUrlParser: true })
+mongoose.connect('mongodb://localhost:27017/node-agency',  { useNewUrlParser: true })
     .then(() => {
        
-        console.log("La conexión a la base de datos curso_mean_social se ha realizado correctamente")
+        console.log("La conexión a la base de datos node-agency se ha realizado correctamente")
 
         app.listen(port, () => {
-            console.log("servidor corriendo en atlas");
+            console.log("servidor corriendo en localhost");
         });
     })
     
